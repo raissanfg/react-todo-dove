e0bdc866f103f2de5f9a6bd4a9baff7c6ada5865
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-03-14 17:18:03 -0500

    assignment-1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
+const list = [
+  {
+    id: 22,
+    title: 'Complete assignment'
+  },
+];
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+        <ul>
+          {list.map(function(item){
+          return <li>{item.id}</li>
+        })}
+          {list.map(function(item){
+          return <li>{item.title}</li>
+        })}
+        </ul>
     </div>
   );
 }
