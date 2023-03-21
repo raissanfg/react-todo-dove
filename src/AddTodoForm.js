526c22c12c81bf28c4b84d0e6f4afa6d05ad8b41
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-03-21 13:20:16 -0500

    lesson1.2

---
+++ src/AddTodoForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function AddTodoForm() {
+    return (
+      <form>
+        <label htmlFor='todoList'>"Title</label>
+        <input type='text' id='todoTitle'></input>
+        <button type='button'>Add</button>
+
+      </form>
+    );
+
+}
--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React from 'react';
+import TodoList from './TodoList.js';
+import AddTodoForm from './AddTodoForm';
 
-const list = [
-  {
-    id: 1,
-    title: 'Complete assignment'
-  },
-  {
-    id: 2,
-    title: 'Assignment in progress'
-  },
-  {
-    id: 3,
-    title: 'Assignment complete'
-  }
-];
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
-        <ul>
-          {list.map(function(item){
-          return <li key={item.id}>{item.title}</li>
-        })}
-        </ul>
+     <TodoList />
+     <AddTodoForm />
     </div>
   );
 }
