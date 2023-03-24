be66ac011acdd12cf5dee71ffc13900796407386
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-03-24 16:35:14 -0500

    lesson2

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 
-const list = [
-  {
-    id: 1,
-    title: 'Complete the assignment!'
-  },
-  {
-    id: 2,
-    title: 'Assignment in progress!'
-  },
-  {
-    id: 3,
-    title: 'Assignment complete!'
-  }
-];
+
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
-      <div>
-        <ul>
-        {list.map(function(item){
-        return <li key={item.id}>{item.title}</li>
-      })}
-      </ul>
-      </div>
+
+      <TodoList />
+      <AddTodoForm />
     </div>
   );
 }
---
+++ src/TodoList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const list = [
+    {
+      id: 1,
+      title: 'Complete the assignment!'
+    },
+    {
+      id: 2,
+      title: 'Assignment in progress!'
+    },
+    {
+      id: 3,
+      title: 'Assignment complete!'
+    }
+  ];
+
+export default function TodoList() {
+    return (
+        <div>
+        <ul>
+        {list.map(function(item){
+        return <li key={item.id}>{item.title}</li>
+      })}
+      </ul>
+      </div>
+    );
+} 
